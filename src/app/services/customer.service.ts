6ebdd7a2fcc4d42311c4291b005573cd5dea71a4
De <tdupondeep@gmail.com>
2021-01-25 15:04:41 -0500

    use back-end

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -4,9 +4,9 @@ import { ICustomer } from '../model/icustomer.interface';
 
 // const apiBaseUrl = "http://localhost:3000";
 // const apiBaseUrl = "http://localhost:30001";
-// const apiBaseUrl = "http://back-end:30001"; //
+const apiBaseUrl = "http://back-end:3000"; //
 // const apiBaseUrl = "http://docker.for.win.localhost:3000"; //
-const apiBaseUrl = "http://10.0.0.219:3000"; //
+// const apiBaseUrl = "http://10.0.0.219:3000"; //
 
 @Injectable({
   providedIn: 'root'
